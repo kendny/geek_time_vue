84df849f869a70669b82a504adef8d0fc8f854fc
xxxian <wh_kendny@163.com>
2019-04-01 21:25:04 +0800

    05.初识单文件组件

---
+++ vue_study/src/App.vue
@@ -0,0 +1,55 @@
+<template>
+    <div id="app">
+        <!-- <img alt="Vue logo" src="./assets/logo.png"> -->
+        <!-- <HelloWorld msg="Welcome to Your Vue.js App"/> -->
+        <input type="text" v-model="info">
+        <button @click="handleClick">add</button>
+        <TodoItem v-for="(item, key) in list" :item="item" v-bind:key="key">
+          <!-- 
+            插槽语法：
+              vue2.6之前的语法： <template slot="slotname"></template>
+              vue2.6之后的语法： <template v-slot:slotname></template>
+           -->
+            <template v-slot:item="itemProps">
+              <span :style="{fontSize: '34px', color:itemProps.checked? 'red': 'blue'}">{{item}}--{{itemProps}}</span>
+            </template>
+        </TodoItem>
+    </div>
+</template>
+
+<script>
+// import HelloWorld from './components/HelloWorld.vue'
+import TodoItem from './components/TodoItem.vue'
+
+export default {
+    name: 'app',
+    components: {
+        TodoItem
+    },
+    data() {
+        return {
+            list: [],
+            info: '',
+            msg: "hello world"
+        }
+    },
+    methods: {
+        handleClick() {
+            console.log(this.list)
+            this.list.push(this.info)
+            this.info = ''
+        }
+    }
+}
+</script>
+
+<style>
+/* #app {
+    font-family: 'Avenir', Helvetica, Arial, sans-serif;
+    -webkit-font-smoothing: antialiased;
+    -moz-osx-font-smoothing: grayscale;
+    text-align: center;
+    color: #2c3e50;
+    margin-top: 60px;
+} */
+</style>
---
+++ vue_study/src/components/TodoItem.vue
@@ -0,0 +1,34 @@
+<template>
+    <!-- <li class="item">{÷{item}}</li> -->
+
+    <!-- 插槽 -->
+    <li class="item">
+        <!-- 作用域插槽的使用场景：将子组件的值或状态通过作用域插槽传递到父组件，进而控制相关的的 -->
+        <input type="checkbox" v-model="checked">
+        <slot name="item" v-bind="{checked}"></slot>
+    </li>
+</template>
+
+<script>
+export default {
+    name: "TodoItem",
+    props: {
+        item: String,
+    },
+    data(){
+        return {
+            checked: false
+        }
+    }
+}
+</script>
+
+
+<style scoped>
+/*scoped: 将全局样式改变为局部样式*/ 
+.item {
+    color: rebeccapurple;
+}
+</style>
+
+
